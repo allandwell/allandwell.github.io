2f8507a79d756a0103a0eb535ebf3c98a0743dee
justpic <matrix.orz@gmail.com>
2021-04-12 22:35:47 +0800

    fi

---
+++ precache-manifest.aab54d8c5d5a84d0874c3d2f5a9ebe84.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6c4ffddc259c93bb023880ed6ae8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28661e6e40d45252e76e",
+    "url": "/static/css/2.cebec226.chunk.css"
+  },
+  {
+    "revision": "bdf0203392b0ae352070",
+    "url": "/static/css/main.defea8b9.chunk.css"
+  },
+  {
+    "revision": "28661e6e40d45252e76e",
+    "url": "/static/js/2.fb398074.chunk.js"
+  },
+  {
+    "revision": "b21df2432fea93eee155b168da918f92",
+    "url": "/static/js/2.fb398074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf0203392b0ae352070",
+    "url": "/static/js/main.ebb7b6dc.chunk.js"
+  },
+  {
+    "revision": "4391ad9a5ac74f0eb4a1",
+    "url": "/static/js/runtime-main.64d553f5.js"
+  },
+  {
+    "revision": "865a0a175c66fd38550835b6c137d8a9",
+    "url": "/static/media/Barlow-Light.865a0a17.ttf"
+  },
+  {
+    "revision": "788f9cc4e37cde7847cd42d30bee07b7",
+    "url": "/static/media/Barlow-Medium.788f9cc4.ttf"
+  },
+  {
+    "revision": "04888aca6941a8880cfcf9543db54c80",
+    "url": "/static/media/icons8-amazon.04888aca.svg"
+  },
+  {
+    "revision": "8816162173b901b522b538efe0d6bf7c",
+    "url": "/static/media/icons8-google.88161621.svg"
+  },
+  {
+    "revision": "9eaf78082d49947232e09b4d48b727be",
+    "url": "/static/media/icons8-menu.9eaf7808.svg"
+  },
+  {
+    "revision": "31c3a5245b160ce1fc59429dac0c1ae2",
+    "url": "/static/media/icons8-search.31c3a524.svg"
+  },
+  {
+    "revision": "be76b3b23dce38460bf2bd4c0d1dd8bf",
+    "url": "/static/media/icons8-trash.be76b3b2.svg"
+  }
+]);
